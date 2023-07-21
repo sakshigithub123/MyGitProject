ed854692470d6df4c7196664fbc2f2d344515667
sakshi <sakshidhakite20@gmail.com>
2023-07-21 10:52:19 -0700

    Adding arraylist

--- src/com/git/project/Demo1.java
+++ src/com/git/project/Demo1.java
@@ -1,10 +1,18 @@
 package com.git.project;
 
+import java.util.ArrayList;
+
 public class Demo1 
 {
 	public static void main(String [] args)
 	{
+		ArrayList<Integer> al = new ArrayList<Integer> ();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		System.out.println(al);
 		System.out.println("Welcome");
 		System.out.println("Welcome to pune");
+		System.out.println("Welcome to git");
 	}
 }
